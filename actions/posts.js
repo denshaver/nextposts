806a7bf59595c0de1a8128b50b5ca3c4c0a08477
Denis Vishchuk <d.vishchuk@wenite.de>
2025-07-27 17:57:13 +0200

    T-177: Cloudinary Image Upload

---
+++ actions/posts.js
@@ -0,0 +1,43 @@
+import { uploadImage } from "@/lib/cloudinary";
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
+export async function createPost(prevState, formData) {
+  "use server";
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || !title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+  if (!content || !content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  let imageUrl;
+
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error("Image upload failed: " + error.message);
+  }
+
+  await storePost({
+    imageUrl,
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+}
